d6f95bc1d0111bb5bf67f31a687b4994e3af864d
kiranverma34 <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>
2026-04-21 08:17:01 +0200

    Custom payment report

---
+++ smr/smr/report/niap_fmt___payments/niap_fmt___payments.js
@@ -0,0 +1,23 @@
+frappe.query_reports["NIAP FMT - PAYMENTS"] = {
+    "filters": [
+        {
+            "fieldname": "party",
+            "label": "Party",
+            "fieldtype": "Data"
+        },
+        {
+            "fieldname": "from_date",
+            "label": "From Date",
+            "fieldtype": "Date",
+            "default": frappe.datetime.month_start(),
+            "reqd": 0
+        },
+        {
+            "fieldname": "to_date",
+            "label": "To Date",
+            "fieldtype": "Date",
+            "default": frappe.datetime.month_end(),
+            "reqd": 0
+        }
+    ]
+};
---
+++ smr/smr/report/niap_fmt___payments/niap_fmt___payments.py
@@ -0,0 +1,52 @@
+import frappe
+
+def execute(filters=None):
+    columns = [
+        {"label": "PYMT_PROD_TYPE_CODE", "fieldname": "party", "fieldtype": "Data", "width": 250},
+        {"label": "PYMT_MODE", "fieldname": "mode_of_payment", "fieldtype": "Data", "width": 120},
+        {"label": "DEBIT_ACC_NO", "fieldname": "bank_account_no", "fieldtype": "Data", "width": 180},
+        {"label": "BNF_NAME", "fieldname": "account_name", "fieldtype": "Data", "width": 180},
+        {"label": "BENE_ACC_NO", "fieldname": "bene_account_no", "fieldtype": "Data", "width": 180},
+        {"label": "BENE_IFSC", "fieldname": "branch_code", "fieldtype": "Data", "width": 180},
+        {"label": "AMOUNT", "fieldname": "paid_amount", "fieldtype": "Currency", "width": 120},
+        {"label": "PYMT_DATE", "fieldname": "posting_date", "fieldtype": "Date", "width": 100},
+        {"label": "REMARK", "fieldname": "remarks", "fieldtype": "Data", "width": 200},
+    ]
+
+    conditions = []
+    values = {}
+
+    if filters.get("from_date") and filters.get("to_date"):
+        conditions.append("pe.posting_date BETWEEN %(from_date)s AND %(to_date)s")
+        values["from_date"] = filters.get("from_date")
+        values["to_date"] = filters.get("to_date")
+
+    if filters.get("party"):
+        conditions.append("pe.party LIKE %(party)s")
+        values["party"] = f"%{filters.get('party')}%"
+
+    where_clause = " AND ".join(conditions)
+    if where_clause:
+        where_clause = " AND " + where_clause
+
+    data = frappe.db.sql(f"""
+        SELECT
+            pe.party,
+            pe.mode_of_payment,
+            ba.bank_account_no,
+            baa.account_name,
+            baa.bank_account_no AS bene_account_no,
+            baa.branch_code,
+            pe.paid_amount,
+            pe.posting_date,
+            pe.remarks
+        FROM `tabPayment Entry` pe
+        LEFT JOIN `tabBank Account` ba 
+            ON ba.name = pe.bank_account
+        LEFT JOIN `tabBank Account` baa  
+            ON baa.name = pe.party_bank_account
+        WHERE pe.docstatus = 1
+        {where_clause}
+    """, values, as_dict=1)
+
+    return columns, data
