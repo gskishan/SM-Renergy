1489df68b31f1b77e85f66c31f4c974440d110f8
G S Kishan <95840962+gskishan@users.noreply.github.com>
2026-07-03 23:06:39 +0530

    Remove Purchase Invoice override from hooks
    Removed override for Purchase Invoice class.

--- smr/hooks.py
+++ smr/hooks.py
@@ -136,9 +136,6 @@ doctype_js = {
 # 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
 
-override_doctype_class = {
-    "Purchase Invoice": "smr.overrides.purchase_invoice.CustomPurchaseInvoice"
-}
 
 # Document Events
 # ---------------
